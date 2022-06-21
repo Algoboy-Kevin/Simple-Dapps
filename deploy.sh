87a5f7e4e8ad921c92eebb61429409163dd4a3f6
Kevin Gamaliel <gamagambogam@gmail.com>
2022-06-21 19:29:13 +0700

    Recommit

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
-# build
+
 npm run build
 
-# navigate into the build output directory
-cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+cd dist
 
 git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Algoboy-Kevin/algoboy-kevin.github.io.git main
+
+git push -f git@github.com:Algoboy-Kevin/Algoboy-kevin.github.io.git main
 
 cd -
