34da1f00c7cb7fa4eba42b75ad3825b6121ad8c8
Kevin Gamaliel <gamagambogam@gmail.com>
2022-06-21 20:32:44 +0700

    Edited deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env sh
 
-set -e
-
 
 npm run build
-
-
-cd dist
-
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:Algoboy-Kevin/Algoboy-kevin.github.io.git main
-
-cd -
+git add dist -f
+git commit -m "updating dist"
+git subtree push --prefix dist origin gh-pages
